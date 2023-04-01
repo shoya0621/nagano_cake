3af92d8ca85a8286e222f128515106131a0a19d4
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-04-01 06:39:56 +0000

    2

--- db/migrate/20230329155023_create_orders.rb
+++ db/migrate/20230329155023_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-
+      t.integer :customer_id, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.integer :shipping_fee, null: false
+      t.integer :payment, null: false
+      t.integer :payment_method, null: false, default: 1
+      t.integer :status, null: false, default: 1
       t.timestamps
     end
   end
--- db/migrate/20230329155033_create_order_details.rb
+++ db/migrate/20230329155033_create_order_details.rb
@@ -1,7 +1,11 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
-
+      t.integer :order_id, null: false, index: true
+      t.integer :item_id, null: false
+      t.integer :quantity, null: false
+      t.integer :purchase_unit_price, null: false
+      t.integer :production_status, null: false, default: 1
       t.timestamps
     end
   end
