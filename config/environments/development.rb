8ae1a9998526bbfdd7492e0c166b263b1e56bdf4
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-04-03 15:32:37 +0000

    6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "dca49d69413749789971848b6af09735.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.web_console.allowed_ips = '0.0.0.0/0'
 end
