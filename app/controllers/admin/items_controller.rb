8e689a4902894bd221c24e4a18e0307e89e13f6f
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-04-16 08:51:43 +0000

    11

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,17 +4,37 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
+
+    
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+  
 end
