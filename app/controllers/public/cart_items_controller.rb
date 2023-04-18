accce7e4c55f508a70cc0b1e40f4183bafc3d21b
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-04-18 15:48:14 +0000

    14

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    
   end
 
   def update
@@ -12,5 +14,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+    
   end
 end
