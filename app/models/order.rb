23e93dde423d18e4fb2c464eeec78c961d714148
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-04-22 04:27:55 +0000

    16

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+  
+  enum payment_method: { credit_card: 1, transfer: 2 }
+  enum status: { waiting: 1, comfirm: 2, making: 3, prepare: 4, deliver: 5}
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,7 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :order
+  belongs_to :item
+  
+  enum production_status: { impossible: 1, wait: 2, making: 3, complete: 4 }
+  
 end
