804e582d43cbc6b1d602e133a2ad393ef0fadedd
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-04-24 15:25:37 +0000

    19

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -46,9 +46,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = Order.where(customer_id: current_customer.id)
+    
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
   
   private
